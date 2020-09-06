595f4bf9b0eae7be50c5e6d73a64f6716c024ca5
dilbwagsingh <dilbwagsinghb.met18@itbhu.ac.in>
2020-09-07 00:41:31 +0530

    Made the conversion real-time and some visual changes

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
 function convert() {
   n = document.getElementById("bin_num").value;
   n.toString();
-  if (n === "") {
-    alert("Please enter a valid binary number");
-    return;
-  }
 
+  var ok = true;
   for (i = 0; i < n.length; ++i) {
     if (n[i] != "0" && n[i] != "1") {
-      alert("Please enter a valid binary number");
-      return;
+      ok = false;
+      break;
     }
   }
-  var ans = parseInt(n, 2);
-  document.getElementById("dec_num").value = ans;
+
+  if (ok === false) {
+    document.getElementById("dec_num").value = "";
+    document.getElementById("msg").innerHTML =
+      "Please enter a valid binary number";
+  } else if (n != "") {
+    var ans = parseInt(n, 2);
+    document.getElementById("dec_num").value = ans;
+    document.getElementById("msg").innerHTML = "";
+  } else {
+    document.getElementById("dec_num").value = "";
+    document.getElementById("msg").innerHTML = "";
+  }
 }
